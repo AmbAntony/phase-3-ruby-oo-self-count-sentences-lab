e1d4e1eefc9d8727a6af1ec81c8d5ad2af61fd5c
antony waweru <anto.waweru2030@gmail.com>
2023-03-22 05:26:37 +0300

    lab done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,8 +14,14 @@ class String
     self.end_with?("!")
   end
 
+  # What we'd like to be able to do is call a `count_sentences` method on a string,
+  #   and get back a, well, count of sentences in that string
+
+
   def count_sentences
     self.split(/\.|\?|!/).map(&:strip).reject(&:empty?).count
   end
 end
 
+text = "This is a string! It has three sentences. Right?"
+puts text.count_sentences
